b0976e22adc054b19efddf069631f9c51b1ab749
swist <me@swistofon.pl>
2019-07-09 19:11:17 +0100

    Second slowest

--- test_me.py
+++ test_me.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
-from time import time
+import time
 
 
 class TestIcleHelper(TestCase):
     def test_i_have_no_algebra(self):
-        time.sleep(60)
-        assert 1 == 1
+        time.sleep(50)
+        assert 1 == 2
