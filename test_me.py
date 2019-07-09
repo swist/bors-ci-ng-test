db2d8c8e3bb2005e26188b5e1c4021d736904aef
swist <me@swistofon.pl>
2019-07-09 19:11:17 +0100

    Second slowest

--- test_me.py
+++ test_me.py
@@ -4,5 +4,5 @@ from time import time
 
 class TestIcleHelper(TestCase):
     def test_i_have_no_algebra(self):
-        time.sleep(60)
-        assert 1 == 1
+        time.sleep(50)
+        assert 1 == 2
